931d04fd5e8ad12ffa11151f59e4240fc2767ca5
Reza Marex <rmotallebi@marex>
2025-09-09 14:36:40 +0200

    first commit

---
+++ src/main/java/com/example/moviewishlist/MovieWishListApplication.java
@@ -0,0 +1,13 @@
+package com.example.moviewishlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieWishListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieWishListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/moviewishlist/MovieWishListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.moviewishlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieWishListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
